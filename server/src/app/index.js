e18b056eef7b07f528001821409c4b0374b8cc8d
BenAgami <benagami80@gmail.com>
2024-07-22 16:04:18 +0300

    feat: added middlewares to the app

--- server/src/app/index.js
+++ server/src/app/index.js
@@ -1,3 +1,17 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
