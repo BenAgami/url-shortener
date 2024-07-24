d310548bc82e69e6aabf666e7ca4bac64b0304d9
BenAgami <benagami80@gmail.com>
2024-07-24 17:34:42 +0300

    build: specified the routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/url", (req, res) => {});
+router.get("/url", async (req, res) => {
+  // #swagger.tags = ['URL']
+  // #swagger.summury = Gets all the objects, their ids, original urls, short urls
+});
 
-router.post("/url", (req, res) => {});
+router.post("/url/createShortUrl/:originalUrl", async (req, res) => {
+  // #swagger.tags = ['URL']
+  // #swagger.summury = Creates a new object which contains everything I wrote above
+});
 
-router.delete("/:shortUrl", (req, res) => {});
+router.get("/url/startWith/:startWith", async (req, res) => {
+  // #swagger.tags = ['URL']
+  // #swagger.summury = Gets all the short urls that starts with a specific word/letter
+});
 
-router.get("/:startWith", (req, res) => {});
+router.get("/url/contains/:contains", async (req, res) => {
+  // #swagger.tags = ['URL']
+  // #swagger.summury = Gets all the short urls that contains a specific word/letter
+});
 
-router.get("/:contains", (req, res) => {});
-
-router.get("/:noContains", (req, res) => {});
+router.get("/url/noContains/:noContains", async (req, res) => {
+  // #swagger.tags = ['URL']
+  // #swagger.summury = Gets all the short urls that contains a specific word/letter
+});
 
 module.exports = router;
