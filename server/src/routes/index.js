6075abee0dd264c6cc81257299223e4aed071071
BenAgami <benagami80@gmail.com>
2024-07-24 14:22:15 +0300

    feat: created the routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("a");
-});
+router.get("/url", (req, res) => {});
 
-module.exports = { router };
+router.post("/url", (req, res) => {});
+
+router.delete("/:shortUrl", (req, res) => {});
+
+router.get("/:startWith", (req, res) => {});
+
+router.get("/:contains", (req, res) => {});
+
+router.get("/:noContains", (req, res) => {});
+
+module.exports = router;
