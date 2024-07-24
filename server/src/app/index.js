4545f250130bcba07b40743ce8e2fde873e43c76
BenAgami <benagami80@gmail.com>
2024-07-24 17:31:57 +0300

    build: deleted redundant components

--- server/src/app/index.js
+++ server/src/app/index.js
@@ -3,23 +3,20 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../swagger");
-// const swaggerDocument = require("../../swagger.json");
+const swaggerDocument = require("../utils/swagger/swagger.json");
+const routers = require("../routes");
+require("../utils/swagger/swagger");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/books", (req, res) => {
-  console.log("kakakakkakak");
-  res.send([{ id: 1, name: "ben" }]);
-});
-
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", routers);
 
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
