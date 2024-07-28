4de8dc9e179c9969b3552a1ab915164fac066c89
BenAgami <benagami80@gmail.com>
2024-07-28 17:44:52 +0300

    build: created sequelize model

---
+++ server/src/utils/models/url.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const { sequelize } = require("../../databases/database");
+
+const Url = sequelize.define("url", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  originalUrl: Sequelize.STRING,
+  shortenerUrl: Sequelize.STRING,
+});
+
+module.exports = Url;
