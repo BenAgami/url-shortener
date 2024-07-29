e9feb453d49f18e9cf15862cce06beabf5442e59
BenAgami <benagami80@gmail.com>
2024-07-29 14:11:49 +0300

    build: deleted files

--- server/src/databases/postgres/createTableQuery.js
+++
@@ -1,7 +0,0 @@
-const tableQuery = `CREATE TABLE IF NOT EXISTS shorter_urls (
-    id SERIAL PRIMARY KEY,
-    original_url varchar(255) UNIQUE,
-    shorter_url varchar(255) UNIQUE
-)`;
-
-module.exports = { tableQuery };
