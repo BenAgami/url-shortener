8b8c9ec1f8570863526901bd7b24b658f2df4945
BenAgami <benagami80@gmail.com>
2024-07-29 11:17:08 +0300

    feat: created models synchronized

--- server/src/databases/database.js
+++ server/src/databases/database.js
@@ -1,16 +1,39 @@
 const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize("url-database", "Ben", "Agami", {
-  host: "localhost",
-  dialect: "postgres",
-});
+dotenv.config();
 
-const connect = async () => {
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT,
+  }
+);
+
+const connectionTest = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
+    process.exit(1);
   }
 };
-module.exports = { sequelize, connect };
+
+const modelSync = async () => {
+  try {
+    await sequelize.sync();
+    console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.log("Unable to synchronize all models:", error);
+  }
+};
+
+const connection = async () => {
+  await connectionTest();
+  await modelSync();
+};
+module.exports = { sequelize, connection };
