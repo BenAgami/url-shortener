c11da47242f61c0c6ab4042b38aeca98dbb3f6d0
BenAgami <benagami80@gmail.com>
2024-07-30 15:41:04 +0300

    test: created get tests

---
+++ server/src/tests/url/url.test.js
@@ -0,0 +1,36 @@
+const { Url, addMockings } = require("../mocking/urlModel");
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const app = require("../../app/index");
+
+chai.use(chaiHTTP);
+
+before(async () => {
+  await Url.destroy({
+    where: {},
+    truncate: true,
+  });
+});
+
+beforeEach(async () => {
+  await addMockings();
+});
+
+describe("Get", () => {
+  describe("Get all urls", () => {
+    it("should return 404 status code if urls not found in db", () => {
+      chai.request(app).get("/url/all").expect(200);
+    });
+    it("should return 404 status code if urls not found in db", async () => {
+      await Url.destroy({
+        where: {},
+        truncate: true,
+      });
+      chai.request(app).get("/url/all").expect(404);
+    });
+    //should return 200 status code and urls if there are in the db
+    it("should return 500 status code", () => {
+      chai.request(app).get("/url/notExist").expect(500);
+    });
+  });
+});
