6449e15eaf74f1c49fce14f1fbf7eb582a660991
BenAgami <benagami80@gmail.com>
2024-08-06 09:55:30 +0300

    feat: changed the model to be more generic

--- server/src/utils/models/url.js
+++ server/src/utils/models/url.js
@@ -1,16 +1,27 @@
 const Sequelize = require("sequelize");
+
 const { sequelizeConnection } = require("../../databases/postgres/database");
 
 const sequelizeUrlDB = sequelizeConnection();
-const UrlModel = sequelizeUrlDB.define("url", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  originalUrl: Sequelize.STRING,
-  shorterUrl: Sequelize.STRING,
-});
 
-module.exports = UrlModel;
+const getUrlModel = () => {
+  const tableName =
+    process.env.NODE_ENV === "test"
+      ? process.env.TESTING_TABLE
+      : process.env.DEV_TABLE;
+
+  const UrlModel = sequelizeUrlDB.define(tableName, {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    originalUrl: Sequelize.STRING,
+    shorterUrl: Sequelize.STRING,
+  });
+
+  return UrlModel;
+};
+
+module.exports = getUrlModel;
